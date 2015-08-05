d7c2183bbba85e56d4be694e7a5143331827012c
carlosrubio <carlos.rubio@gft.com>
2015-08-05 18:21:13 +0200

    disable any option to use chrome

--- test/karma-e2e.conf.js
+++ test/karma-e2e.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
 
     conf.coverageReporter.dir += 'e2e';
 
-    conf.browsers = ['Chrome'];
+    conf.browsers = ['PhantomJS'];
 
     conf.files = [
         'app/bower_components/appverse-web-html5-core/dist/appverse/appverse.min.js',
